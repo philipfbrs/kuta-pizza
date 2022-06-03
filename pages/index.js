0af615aa3cf33020219ab7ad176ddcdcd1fd09da
philipfbrs <philipfbrs@gmail.com>
2022-06-03 16:54:34 +0800

    added preload v3

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,20 @@ export default function Home({ allPostsData }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="overflow-hidden">
+    <>
       <Head>
         <link
           rel="preload"
           as="image"
-          crossOrigin="anonymous"
           href="bg_main_tqFShJ5ri.jpg"
         />
       </Head>
-      <Header setOpen={setOpen} open={open} />
-      <Main allPostsData={allPostsData} />
-      <Product />
-      <Footer />
-    </div>
+      <div className="overflow-hidden">
+        <Header setOpen={setOpen} open={open} />
+        <Main allPostsData={allPostsData} />
+        <Product />
+        <Footer />
+      </div>
+    </>
   );
 }
