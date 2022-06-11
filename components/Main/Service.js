c2d09268a57fbdf0ac27a0b6bab8a30775bd92fd
philipfbrs <philipfbrs@gmail.com>
2022-06-11 13:30:43 +0800

    added service v2

--- components/Main/Service.js
+++ components/Main/Service.js
@@ -5,10 +5,13 @@ import { FaMotorcycle } from "react-icons/fa";
 import {IoMdPizza} from "react-icons/io";
 export const Service = () => {
   return (
+    <div name="service">
+    <h1 className="flex text-center  justify-center w-screen bg-slate-200 py-10 text-4xl font-light">Want to eat pizza?</h1>
     <div className="flex flex-col md:flex-row items-center justify-center w-screen h-fit bg-slate-200 text-[#91461d] p-8 gap-4">
       <ServiceItems Item = {MdPhoneInTalk} title="Call" desc = "Call our hotline: 7911-22-33"/>
       <ServiceItems Item = {FaMotorcycle} title="Deliver" desc="Delivery fast"/>
-      <ServiceItems Item = {IoMdPizza} title="Eat" desc="Eat well!"/>
+      <ServiceItems Item = {IoMdPizza} title="Eat" desc="fill your tummy with this delicious pizza"/>
+    </div>
     </div>
   );
 };
